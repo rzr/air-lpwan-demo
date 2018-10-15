b245b62c807553678e6600af621fb3944ebb618f
Philippe Coval <p.coval@samsung.com>
2018-10-15 22:03:19 +0200

    serialport: Backport to support TizenRT's IoT.js
    Fix UART instance
    
    This prevent a crash on write
    
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- iotjs/serialport.js
+++ iotjs/serialport.js
@@ -15,10 +15,11 @@
  */
 
 var EventEmitter = require('events').EventEmitter;
-var uart = require('uart');
+var Uart = require('uart');
+var uart = new Uart();
 
 function log(arg) {
-  if (true)
+  if (true)    
     return; // TODO: silent mode if !true
   try {
     console.log("log: serialport: " + JSON.stringify(arg));
@@ -28,40 +29,40 @@ function log(arg) {
   }
 }
 
-function die(err, message) {
-  var text = "error: fatal: " + message;
-  text += err.message; // if exeception
-  log("die:");
-  log(text);
-  process.exit();
-  return text;
-}
-
 function SerialPort(device, configuration) {
   configuration.device = device;
   var self = this;
+  EventEmitter.call(this);
+  this.configuration = configuration;
+  this.onError = function(err) {
+    log('error: ' + err);
+    self.emit("error", err);
+  }
 
-  self.onError = function(err) { die(err, "onError"); }
-
-  EventEmitter.call(self);
-  self.uart = uart.open(configuration, function(err) {
+  log('About to open: ' + configuration.device);
+  this.uart = uart.open(configuration, function(err) {
+    log('opened: ' + err);
     if (err) {
       self.emit("error", err);
     } else {
+      self.uart.on('error', self.onError);
       self.uart.on('data', function(data) {
-	self.emit("data", data.toString());
+        log('data: ' + data);
+	self.emit("data", data && data.toString());
       });
     }
   });
 
-  return self;
+  return this;
 }
 
 SerialPort.prototype = Object.create(require('events').EventEmitter.prototype);
 
 SerialPort.prototype.write = function write(message, callback) {
-  var self = this;
-  self.uart.write(message, callback);
+  log('About to write: ' +  message);
+  if ( this.uart && this.uart.write) {
+    this.uart.write(message, callback);
+  }
 }
 
 module.exports = SerialPort;
